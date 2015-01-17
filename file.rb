e7a528392c48ba28a1cfbf45071c91ca0dd5d138
Max Bernstein <tekknolagi@gmail.com>
2015-01-17 03:33:45 -0500

    sample filename

--- file.rb
+++ file.rb
@@ -2,6 +2,7 @@ class File
   def chunk fn, prefix, chunksize = 4_194_304 # 4MB
     File.open(fn, 'r') do |file|
       until file.eof?
+        # sample: f572d396fae9206628714fb2ce00f72e94f2258f_00001
         File.open("#{prefix}_#{"%05d"%(file.pos/chunksize)}", "w") do |file_out|
           file_out << file.read(chunksize)
         end
