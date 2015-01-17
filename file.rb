ea0ba9bf40987a74533fc5d9242c3d5296ccce02
theahura <theahura@gmail.com>
2015-01-17 04:02:05 -0500

    Just added a comment

--- file.rb
+++ file.rb
@@ -2,8 +2,9 @@ class File
   def chunk fn, prefix, chunksize = 4_194_304 # 4MB
     File.open(fn, 'r') do |file|
       until file.eof?
-        # sample: f572d396fae9206628714fb2ce00f72e94f2258f_00001
+        # sample: f572d396fae9206628714fb2ce00f72e94f2258f_00001 (the sha1 is for file)
         File.open("#{prefix}_#{"%05d"%(file.pos/chunksize)}", "w") do |file_out|
+        	# returns list of chunks with their sha1's and refs
           file_out << file.read(chunksize)
         end
       end
