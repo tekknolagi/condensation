61650a7ac75285a45db284da74b27230596180bb
Arun Drelich <ad2476@gmail.com>
2015-01-17 22:38:54 -0500

    App sets up local environment if not exist

--- config.rb
+++ config.rb
@@ -5,6 +5,20 @@ class Konfig
   attr_accessor :db
 
   def initialize
+    if not Dir.exists?('~/.condensation')
+      Dir.mkdir '~/.condensation'
+    end
+    if not File.exist?('~/.condensation/api.json')
+      f = File.open(File.expand_path '~/.condensation/api.json', 'w')
+      f.write { :dropbox => '', :onedrive => '', :box => '', :flickr => '', :google => ''}.to_json
+      f.close
+    end
+    if not File.exist?('~/.condensation/db.json')
+      f = File.open(File.expand_path '~/.condensation/db.json', 'w')
+      f.write { :fn2ref => {}, :chunk2ref => {} }.to_json
+      f.close
+    end
+
     @@api_path = File.expand_path '~/.condensation/api.json'
     @@db_path = File.expand_path '~/.condensation/db.json'
     read
