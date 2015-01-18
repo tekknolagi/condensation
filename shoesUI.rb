2b35f97d4906f4f85f2ae80538eeb1e16d1bcda6
theahura <theahura@gmail.com>
2015-01-17 22:43:34 -0500

    Updates to the shoes UI to make it more usable
    Signed-off-by: theahura <theahura@gmail.com>

--- shoesUI.rb
+++ shoesUI.rb
@@ -1,34 +1,84 @@
-require './file'
-require './config'
-require './provider'
-require './condense'
+#require './file'
+#require './config'
+#require './provider'
+#require './condense'
 
-condenser = Condense.new
+#app = Condense.new
 
-Shoes.app {
-  puts "HELLO"
 
-  @note = para ""
-  
-  stack {
-    flow do 
-      @filename = edit_line
-      @upload = button "Upload"
-    end
-    @download = button "Download"
-  }
+Shoes.app(title: "Condenser",
+   width: 190, height: 600, resizable: false) {
 
-  @upload.click{
-    if not @filename
-      puts "Messed up"
-    end
-    puts condenser.file_put @filename
-  }
 
   stack {
-    button "Add Google Account"
-    button "Add Dropbox Account"
-    button "Add OneDrive Account"
+    @note = para "Hello"
+    @upload = button "       Upload       "
+    @download = button "      Download      "
+    @Google = button "Add Google Account  "
+    @Dropbox = button "Add Dropbox Account "
+    @OneDrive = button "Add OneDrive Account"
+
+    @amountLeft = para ""
+    @amountBar = progress width: 1.0
+
   }
+
+  @upload.click {
+    filepath = ask("Please Enter a File Path to Upload a File")
+    if not filepath
+      @note.replace "There is no file path listed"
+      return
+    end
+
+    app.file_put filepath
+
+    #check to make sure these are the right function names for total size in cloud and amount used in cloud
+    print_current_size condense.get_total_size condense.get_amount_used
+  }
+
+  @download.click {
+    filename = ask("Please Enter a File Name to Download a File")
+    if not filename
+      @note.replace "There is no file name listed"
+      return
+    end
+
+    app.file_get filename
+  }
+
+  @Google.click {
+    app.configure Google
+  }
+
+  @Dropbox.click {
+    app.configure Dropbox
+  }
+
+  @OneDrive.click {
+    app.configure OneDrive
+  }
+
+  def print_file_list
+    list = app.file_list
+
+    #Not sure if this works like it is supposed to
+    list.each do |filename, properties|
+      @temp = button filename
+      @temp.click {
+        app.file_get filename
+      }
+    end
+  end
+
+
+  def print_current_size total_size amount_used
+    percent_used = amount_used/total_size
+    amount_left = total_size - amount_used
+    @amountBar.fraction = percent_used
+    amount_left = amount_left + " mb"
+    @amountLeft.replace amount_left
+  end
+
+  print_file_list #generates the files below the rest of the buttons
 
 }
