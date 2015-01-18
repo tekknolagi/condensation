a243ea0751a8709dae07aea851e4e419ecf85b23
Max Bernstein <tekknolagi@gmail.com>
2015-01-17 21:44:13 -0500

    strip quotes

--- provider/box.rb
+++ provider/box.rb
@@ -5,25 +5,24 @@ require 'launchy'
 class Provider ; end
 
 class BoxService < Provider
-	AUTH_URL = 'http://condensation-auth.herokuapp.com/box'
-	attr_accessor :access_token
+  AUTH_URL = 'http://condensation-auth.herokuapp.com/box'
+  attr_accessor :access_token
 
-	puts "one"
-
-	def get_token
-		# Make GET to auth server/build_url
-		authorize_url_json = Net::HTTP.post_form(URI("#{AUTH_URL}/build_url"), {})
+  def get_token
+    # Make GET to auth server/build_url
+    authorize_url_json = Net::HTTP.post_form(URI("#{AUTH_URL}/build_url"), {})
     parsed_json = JSON.parse(authorize_url_json.body)
     authorize_url = parsed_json['authorize_url']
 
-		# Launchy(authorize_url)
-		Launchy.open(authorize_url)
-		puts "Please paste the code from the browser."
-		code = gets.chomp
+    # Launchy(authorize_url)
+    Launchy.open(authorize_url)
+    puts 'Please authorise Condensation for your Box account:'
+    puts '1. Go to: ' + authorize_url
+    puts '2. Click "Allow" (you might have to log in first)'
+    puts '3. Copy the authorization code'
+    print 'Enter the authorization code here: '
+    code = gets.strip.gsub("\"","")
 
-		# Send code to Box for access_token
-					
-	end
-
-	puts "end"
+    # Send code to Box for access_token
+  end
 end
