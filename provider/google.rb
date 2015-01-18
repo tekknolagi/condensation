1d7bcf72db7c7fecea439bb18f31a2109d8dde75
theahura <theahura@gmail.com>
2015-01-18 02:09:53 -0500

    Retrying original code with some extra output
    Signed-off-by: theahura <theahura@gmail.com>

--- provider/google.rb
+++ provider/google.rb
@@ -2,10 +2,6 @@ require 'json'
 require 'launchy'
 require 'google/api_client'
 
-require 'rubygems'
-
-
-
 class Provider; end
 
 class GoogleService < Provider
@@ -14,52 +10,49 @@ class GoogleService < Provider
   OAUTH_SCOPE = 'https://www.googleapis.com/auth/drive'
   REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
 
-  API_VERSION = 'v2'
-  CACHED_API_FILE = "drive-#{API_VERSION}.cache"
-  CREDENTIAL_STORE_FILE = "#{$0}-oauth2.json"
-
-  @client
-
   attr_accessor :access_token
 
   # There are some issues with trying to split this up into a proper client-server for client auth
   # See this: https://developers.google.com/drive/web/quickstart/quickstart-ruby
   def get_token
     create_client
-   
-    uri = client.authorization.authorization_uri
-    Launchy.open(uri)
+    authorize_url = @client.authorization.authorization_uri
 
-    # Exchange authorization code for access token
-    $stdout.write  "Enter authorization code: "
-    client.authorization.code = gets.chomp
-    client.authorization.fetch_access_token!
+    # Have the user sign in and authorize this app
+    Launchy.open(authorize_url)
+    puts 'Please authorise Condensation for your Google account:'
+    puts '1. Go to: ' + authorize_url
+    puts '2. Click "Allow" (you might have to log in first)'
+    puts '3. Copy the authorization code'
+    print 'Enter the authorization code here: '
+    @client.authorization.code = gets.chomp
+    @client.authorization.fetch_access_token!
 
-    puts client.authorization.code
+    puts @client.authorization.code
+
+    # At this point I believe the client is all set up (authenticated and whatnot)
+    # To do is still: Figure out how we can avoid doing all this all over each time app.rb is run
   end
 
   def create_client
-      puts "2.1"
-
     # Create a new API client & load the Google Drive API
     @client = Google::APIClient.new({ :application_name => 'Condensation', :application_version => '0.0.0' })
     @drive = @client.discovered_api('drive', 'v2')
-    puts "2.2"
+
     # POST request to auth server to get JSON of api keys
     secrets = Net::HTTP.get(URI.parse("#{AUTH_SERVER}/secrets"))
     api_secrets = JSON.parse(secrets)
     client_id = api_secrets['key']
     client_secret = api_secrets['secret']
-      puts "2.3"
+
     # Request authorization
     @client.authorization.client_id = client_id
     @client.authorization.client_secret = client_secret
     @client.authorization.scope = OAUTH_SCOPE
     @client.authorization.redirect_uri = REDIRECT_URI
     @client.authorization.access_token = @access_token if @access_token
-    puts "2.4"
   end
-end
+
 #   def file_get fn
 #     create_client
 #     file_properties = json_file_data[fn]
@@ -115,13 +108,7 @@ end
 #     else
 #       puts "There was an error inserting your file to drive"
 #       return false
-
-#     #RETURN FILE ID
 #     end
-#   end
-
-#   def file_del fn fid
-#     puts "what"
 #   end
 
 #   def space_free
