9744400ca780d68e006a80fd9abcce4de13800f6
Arun Drelich <ad2476@gmail.com>
2015-01-18 03:05:31 -0500

    Reformatted some help text

--- app.rb
+++ app.rb
@@ -9,22 +9,22 @@ app = Condense.new
 
 OptionParser.new do |opts|
   opts.banner += " [arguments...]"
-  opts.separator "This will parse commands to be used by Condensation."
+  opts.separator "This will parse commands to be used by Condensation"
   opts.version = "0.0.0"
 
-  opts.on("-c", "--configure [SERVICE]", "Sign into your services.") do |svc|
+  opts.on("-c", "--configure [SERVICE]", "Sign into your services (dropbox | onedrive | box | google)") do |svc|
     app.configure svc
   end
 
-  opts.on("-l", "--list", "Returns list of files in the cloud.") do
+  opts.on("-l", "--list", "Returns list of files in the cloud") do
     puts app.file_list
   end
 
-  opts.on("-u", "--upload FILEPATH", "Uploads the file at FILEPATH.") do |args|
+  opts.on("-u", "--upload FILEPATH", "Uploads the file at FILEPATH") do |args|
     app.file_put args
   end
 
-  opts.on("-s", "--space [SERVICE]", "Get the amount of free space from a provider(s).") do |svc|
+  opts.on("-s", "--space [SERVICE]", "Get the amount of free space from a provider(s)") do |svc|
     if not svc
       puts app.get_cloud_usage
     else
@@ -32,11 +32,11 @@ OptionParser.new do |opts|
     end
   end
 
-  opts.on("-d", "--download FILENAME", "Downloads the file named FILENAME.") do |args|
+  opts.on("-d", "--download FILENAME", "Downloads the file named FILENAME") do |args|
     puts app.file_get args
   end
 
-  opts.on("-x", "--delete FILENAME", "Deletes the file named FILENAME.") do |args|
+  opts.on("-x", "--delete FILENAME", "Deletes the file named FILENAME") do |args|
     puts app.file_del args
   end
 
