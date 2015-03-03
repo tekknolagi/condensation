4464ba90b2b86a0046a9f16817b869ad5813918e
Max Bernstein <tekknolagi@gmail.com>
2015-03-03 09:38:39 -0500

    Use variables!!

--- config.rb
+++ config.rb
@@ -4,23 +4,28 @@ class Konfig
   attr_accessor :keys
   attr_accessor :db
 
+  @@cond = '~/.condensation'
+  @@api_json = '~/.condensation/api.json'
+  @@db_json  = '~/.condensation/db.json'
+
   def initialize
-    if not Dir.exists?(File.expand_path '~/.condensation')
-      Dir.mkdir File.expand_path('~/.condensation')
+    if not Dir.exists?(File.expand_path @@cond)
+      Dir.mkdir File.expand_path(@@cond)
     end
-    if not File.exist?(File.expand_path '~/.condensation/api.json')
-      f =File.open(File.expand_path('~/.condensation/api.json'), 'w')
-      f.write({ :dropbox => {}, :onedrive => {}, :box => {}}.to_json) # when ready, make sure this includes google as well
+    if not File.exist?(File.expand_path @@api_json)
+      f = File.open(File.expand_path(@@api_json), 'w')
+      # when ready, make sure this includes google as well
+      f.write({ :dropbox => {}, :onedrive => {}, :box => {} }.to_json)
       f.close
     end
-    if not File.exist?(File.expand_path '~/.condensation/db.json')
-      f = File.open(File.expand_path('~/.condensation/db.json'), 'w')
+    if not File.exist?(File.expand_path @@db_json)
+      f = File.open(File.expand_path(@@db_json), 'w')
       f.write({ :fn2ref => {}, :chunk2ref => {} }.to_json)
       f.close
     end
 
-    @@api_path = File.expand_path '~/.condensation/api.json'
-    @@db_path = File.expand_path '~/.condensation/db.json'
+    @@api_path = File.expand_path @@api_json
+    @@db_path = File.expand_path @@db_json
     read
   end
 
